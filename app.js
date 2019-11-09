2b656a80a67a23eec76585a033a1ba62c384ea7c
Bryan Smith <bryanscarolina@gmail.com>
2019-11-08 19:03:42 -0500

    really restart the clock this time. oops

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const data = {
   dollars: null,
   cents: null,
   dailyCost: 5.375466667,
-  time: 1573230600 // new Date(2019, 11, 08, 19 at 1630)
+  time: 1573317143 // new Date(2019, 11, 08, 19 at 1630)
 };
 
 const progresses = document.querySelectorAll(".progress[fraction]");
